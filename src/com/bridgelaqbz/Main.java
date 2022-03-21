9cc04062ccd2d8993247025fa5caca84ecb5906f
jayant <jayantparmar420@gmail.com>
2022-03-21 09:54:38 +0530

    added the first branch

--- src/com/bridgelaqbz/Main.java
+++ src/com/bridgelaqbz/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("welcome to the demo work");
+        System.out.println("this is the branch 1 ");
     }
 }
