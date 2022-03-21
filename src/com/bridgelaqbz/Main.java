fe4241da1e8b6ac56b0a6eb0b4f3359d3bbd8c53
jayant <jayantparmar420@gmail.com>
2022-03-21 22:59:47 +0530

    added branch 2

--- src/com/bridgelaqbz/Main.java
+++ src/com/bridgelaqbz/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("welcome to the demo work");
         System.out.println("this is the branch 1 ");
+        System.out.println( " this is branch 2");
+
     }
 }
